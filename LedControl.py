ad7eb54aa702fa862dcff69ad111d76059cce479
Brudasoe <serafadam@gmail.com>
2019-06-09 17:02:46 +0200

    get pixel values from desktop

--- LedControl.py
+++ LedControl.py
@@ -2,6 +2,11 @@ import sys
 import board
 import neopixel
 import time
+import numpy as np
+import gi
+gi.require_version('Gdk', '3.0')
+from gi.repository import Gdk
+import numpy as np
 
 
 class LedControl(object):
@@ -9,6 +14,10 @@ class LedControl(object):
         self.board_pin = board.D18
         self.pixel_num = 60
         self.pixels = neopixel.NeoPixel(self.board_pin, self.pixel_num)
+        self.pixel_screen_positions = np.arange(0,1920,32)
+        self.w = Gdk.get_default_root_window()
+        self.pixel_values = np.zeros((60,3),dtype=np.uint8)
+        
 
     def pixel_rotation(self):
         for i in range(0,self.pixel_num):
@@ -21,10 +30,25 @@ class LedControl(object):
     def turn_off(self):
         self.pixels.fill(0)
 
+    def get_pixel(self):
+        count = 0
+        for i in self.pixel_screen_positions:
+            pb = Gdk.pixbuf_get_from_window(self.w, i, 1000, 1, 1)
+            self.pixel_values[count] = np.frombuffer(pb.get_pixels(), dtype=np.uint8)
+            count+=1
+
+    def light_pixel_array(self):
+        print(self.pixels[0])
+        for i in range(self.pixel_num):
+            self.pixels[i] = tuple(map(tuple,self.pixel_values))[i]
+
+    def run_controller(self):
+        self.get_pixel()
+        self.light_pixel_array()
+
 
 if __name__ == '__main__':
     led_controller = LedControl()
     led_controller.turn_off()
-    for i in range(0,15):
-        led_controller.pixel_rotation()
-        led_controller.turn_off()
+    while(True):
+        led_controller.run_controller()
