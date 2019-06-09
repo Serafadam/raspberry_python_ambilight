228c7328c750051a4d401b4e522df35dff737ff9
Brudasoe <serafadam@gmail.com>
2019-06-09 22:00:01 +0200

    changed resolution, added keyboard interrupt

--- LedControl.py
+++ LedControl.py
@@ -14,7 +14,7 @@ class LedControl(object):
         self.board_pin = board.D18
         self.pixel_num = 60
         self.pixels = neopixel.NeoPixel(self.board_pin, self.pixel_num)
-        self.pixel_screen_positions = np.arange(0,1920,32)
+        self.pixel_screen_positions = np.arange(0,1280,1280/self.pixel_num)
         self.w = Gdk.get_default_root_window()
         self.pixel_values = np.zeros((60,3),dtype=np.uint8)
         
@@ -33,22 +33,27 @@ class LedControl(object):
     def get_pixel(self):
         count = 0
         for i in self.pixel_screen_positions:
-            pb = Gdk.pixbuf_get_from_window(self.w, i, 1000, 1, 1)
+            pb = Gdk.pixbuf_get_from_window(self.w, i, 500, 1, 1)
             self.pixel_values[count] = np.frombuffer(pb.get_pixels(), dtype=np.uint8)
             count+=1
 
     def light_pixel_array(self):
-        print(self.pixels[0])
         for i in range(self.pixel_num):
             self.pixels[i] = tuple(map(tuple,self.pixel_values))[i]
 
     def run_controller(self):
-        self.get_pixel()
-        self.light_pixel_array()
+        while(True):
+            try:
+                self.get_pixel()
+                self.light_pixel_array()
+            except KeyboardInterrupt:
+                led_controller.turn_off()
+                break
 
 
 if __name__ == '__main__':
     led_controller = LedControl()
     led_controller.turn_off()
-    while(True):
-        led_controller.run_controller()
+    
+    led_controller.run_controller()
+        
